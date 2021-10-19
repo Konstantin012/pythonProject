1065ad714dab30257504c816376b34c1a3f60241
KoS <kkon870@gmail.com>
2021-10-19 22:59:30 +0300

    selektors

--- for_interviw/Singleton.py
+++ for_interviw/Singleton.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class WebDriver:
 
     class __WebDriver:
         def __init__(self):
-            servise = Service("for_interviw/chromedriver.exe")
+            servise = Service("C:\\Users\\KoS\\PycharmProjects\\pythonProject\\for_interviw\\chromedriver.exe")
             self.driver = webdriver.Chrome(service=servise)
 
-    driver = None
+    driver:WebDriver = None
 
     def __init__(self):
         if not self.driver:
@@ -22,7 +23,8 @@ class WebDriver:
 def website_opener():
     d = WebDriver().driver
     # driver.get('https://google.com')
-    driver2 = WebDriver().driver
-
-
+    # driver2 = WebDriver().driver
+    d.get('https://selenium-python.com/')
+    element = d.find_element(By.XPATH, "//h2[text()='Python + Selenium']")
+    element.click()
 website_opener()
